df5257252ca6a93f7a8fbf8e100b1b7c62ebbdac
Brent <bcen0001@student.monash.edu>
2024-05-04 16:19:29 +1000

    Added db to the repository to be able to retrieve the firestore instance easily across the application.

--- app/src/main/java/com/brentcodes/fitfamapplication/repo/AuthRepository.kt
+++ app/src/main/java/com/brentcodes/fitfamapplication/repo/AuthRepository.kt
@@ -2,6 +2,7 @@ package com.brentcodes.fitfamapplication.repo
 
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.StateFlow
@@ -10,6 +11,7 @@ import retrofit2.Response
 interface AuthRepository {
     val currentUser: Flow<FirebaseUser?>
     val currentUserId: String
+    val db: FirebaseFirestore
     fun hasUser(): Boolean
     suspend fun signIn(email: String, password: String)
     suspend fun signUp(email: String, password: String)
--- app/src/main/java/com/brentcodes/fitfamapplication/repo/AuthRepositoryImpl.kt
+++ app/src/main/java/com/brentcodes/fitfamapplication/repo/AuthRepositoryImpl.kt
@@ -7,6 +7,7 @@ import com.google.firebase.app
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.auth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.firestore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
@@ -23,6 +24,9 @@ class AuthRepositoryImpl @Inject constructor(): AuthRepository {
     override val currentUserId: String
         get() = Firebase.auth.currentUser?.uid.orEmpty()
 
+    override val db: FirebaseFirestore
+        get() = Firebase.firestore
+
     override fun hasUser(): Boolean {
         return Firebase.auth.currentUser != null
     }
@@ -30,17 +34,15 @@ class AuthRepositoryImpl @Inject constructor(): AuthRepository {
     override suspend fun signIn(email: String, password: String) {
         Firebase.auth.signInWithEmailAndPassword(email, password).await()
         Log.d("SignIn", "Signing in with $email and $password")
-        /*val user = currentUserId
+        val user = currentUserId
         Log.d("SignIn", "Current user: $user")
-        val db = Firebase.firestore
+
         val data = mapOf("name" to "Brent Cenci", "level" to 12)
         if (user.isNotEmpty()) {
             //do something to generate user file?
-            Log.d("SignIn", "Attempting to create db collection")
-            db.collection("users").add(user).addOnSuccessListener {
-                it.set(data)
-            }
-        }*/
+            Log.d("SignIn", "Attempting to create db collection with db : ${db.collection("users").path}")
+            db.collection("users").document(user).set(data)
+        }
 
     }
 
