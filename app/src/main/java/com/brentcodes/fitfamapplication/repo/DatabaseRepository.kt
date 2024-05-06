59f37c8db6f271bacc53e46e64fcee9ee2135b93
Brent <bcen0001@student.monash.edu>
2024-05-07 01:50:47 +1000

    More functions added and tested accessing current user to display dynamic data; i.e. Month the user signed up

--- app/src/main/java/com/brentcodes/fitfamapplication/repo/DatabaseRepository.kt
+++ app/src/main/java/com/brentcodes/fitfamapplication/repo/DatabaseRepository.kt
@@ -5,5 +5,25 @@ import com.google.firebase.firestore.FirebaseFirestore
 
 interface DatabaseRepository {
     val db: FirebaseFirestore
+
+    ////EXERCISE FUNCTIONS////
+    //suspend fun addExercise(exercises: List<Exercise>, name: String, desc: String)
+
+    //suspend fun removeExercise(workoutId: String)
+
+    //suspend fun editExercise(workoutId: String, exercises: List<Exercise>, name: String, desc: String)
+
+
+    ////WORKOUT FUNCTIONS////
     suspend fun addWorkout(exercises: List<Exercise>, name: String, desc: String)
+
+    //suspend fun removeWorkout(workoutId: String)
+
+    //suspend fun editWorkout(workoutId: String, exercises: List<Exercise>, name: String, desc: String)
+
+
+
+    ////SCHEDULED WORKOUT FUNCTIONS////
+
+
 }
--- app/src/main/java/com/brentcodes/fitfamapplication/ui/screens/profile/ProfileScreen.kt
+++ app/src/main/java/com/brentcodes/fitfamapplication/ui/screens/profile/ProfileScreen.kt
@@ -1,5 +1,7 @@
 package com.brentcodes.fitfamapplication.ui.screens.profile
 
+import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,12 +17,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.brentcodes.fitfamapplication.ui.theme.BackgroundGray
 import com.brentcodes.fitfamapplication.ui.theme.DarkerGray
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 import com.patrykandpatrick.vico.compose.axis.horizontal.rememberBottomAxis
 import com.patrykandpatrick.vico.compose.axis.vertical.rememberStartAxis
 import com.patrykandpatrick.vico.compose.chart.CartesianChartHost
@@ -28,8 +33,12 @@ import com.patrykandpatrick.vico.compose.chart.layer.rememberColumnCartesianLaye
 import com.patrykandpatrick.vico.compose.chart.rememberCartesianChart
 import com.patrykandpatrick.vico.core.model.CartesianChartModelProducer
 import com.patrykandpatrick.vico.core.model.columnSeries
+import io.grpc.Context
+import java.sql.Timestamp
+import java.time.Instant
 import java.time.Month
 import java.time.YearMonth
+import java.time.ZoneId
 import java.time.format.TextStyle
 import java.util.Locale
 
@@ -39,7 +48,13 @@ fun ProfileScreen() {
     //RECENT WORKOUTS
 
     val name = "Brent Cenci"
-    val joined = YearMonth.now().minusMonths(1)
+    val monthJoinedTimestamp = Firebase.auth.currentUser?.metadata?.creationTimestamp
+    val joined = Instant.ofEpochMilli(monthJoinedTimestamp?: 1).atZone(ZoneId.systemDefault())
+    val context = LocalContext.current
+
+    //Toast.makeText(context, "User: ${Firebase.auth.currentUser?.email}", Toast.LENGTH_SHORT).show()
+    //Toast.makeText(context, "User: ${Firebase.auth.currentUser?.metadata?.creationTimestamp}", Toast.LENGTH_SHORT).show()
+    //Log.d("User", monthJoinedTimestamp.toString())
 
 
     Box(
