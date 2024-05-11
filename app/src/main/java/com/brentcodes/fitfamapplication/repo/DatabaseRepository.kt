66a37373d783f72b361fa546c4777800a62617da
Brent <bcen0001@student.monash.edu>
2024-05-11 17:54:51 +1000

    Small db repo changes

--- app/src/main/java/com/brentcodes/fitfamapplication/repo/DatabaseRepository.kt
+++ app/src/main/java/com/brentcodes/fitfamapplication/repo/DatabaseRepository.kt
@@ -1,6 +1,8 @@
 package com.brentcodes.fitfamapplication.repo
 
 import com.brentcodes.fitfamapplication.model.Exercise
+import com.brentcodes.fitfamapplication.model.ScheduledWorkout
+import com.brentcodes.fitfamapplication.model.Workout
 import com.google.firebase.firestore.FirebaseFirestore
 import java.util.Date
 
@@ -8,6 +10,7 @@ interface DatabaseRepository {
     val db: FirebaseFirestore
 
     ////EXERCISE FUNCTIONS////
+    suspend fun getExercises(workoutId: String): List<Exercise>
     suspend fun addExercise(name: String, reps: Int, sets: Int)
 
     suspend fun removeExercise(exerciseId: String)
@@ -16,6 +19,7 @@ interface DatabaseRepository {
 
 
     ////WORKOUT FUNCTIONS////
+    suspend fun getWorkouts(): List<Workout>
     suspend fun addWorkout(exercises: List<Exercise>, name: String, desc: String)
 
     suspend fun removeWorkout(workoutId: String)
@@ -25,6 +29,7 @@ interface DatabaseRepository {
 
 
     ////SCHEDULED WORKOUT FUNCTIONS////
+    suspend fun getScheduledWorkouts(): List<ScheduledWorkout>
     suspend fun scheduleWorkout(workoutId: String, date: Date)
 
     suspend fun removeScheduledWorkout(scheduledWorkoutId: String)
--- app/src/main/java/com/brentcodes/fitfamapplication/repo/DatabaseRepositoryImpl.kt
+++ app/src/main/java/com/brentcodes/fitfamapplication/repo/DatabaseRepositoryImpl.kt
@@ -1,8 +1,12 @@
 package com.brentcodes.fitfamapplication.repo
 
+import android.util.Log
 import com.brentcodes.fitfamapplication.model.Exercise
+import com.brentcodes.fitfamapplication.model.ScheduledWorkout
+import com.brentcodes.fitfamapplication.model.Workout
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Source
 import com.google.firebase.firestore.firestore
 import java.util.Date
 import javax.inject.Inject
@@ -10,6 +14,12 @@ import javax.inject.Inject
 class DatabaseRepositoryImpl @Inject constructor(): DatabaseRepository {
     override val db: FirebaseFirestore
         get() = Firebase.firestore
+
+    override suspend fun getExercises(workoutId: String): List<Exercise> {
+        val workoutDoc = db.collection("workouts").document(workoutId)
+        Log.d("db", "workout doc data: ${workoutDoc.get().result.data}")
+        return emptyList()
+    }
 
     override suspend fun addExercise(name: String, reps: Int, sets: Int) {
         val data = mapOf("name" to name, "reps" to reps, "sets" to sets)
@@ -26,6 +36,10 @@ class DatabaseRepositoryImpl @Inject constructor(): DatabaseRepository {
     override suspend fun editExercise(exerciseId: String, name: String, reps: Int, sets: Int) {
         val data = mapOf("name" to name, "reps" to reps, "sets" to sets)
         db.collection("exercises").document(exerciseId).set(data)
+    }
+
+    override suspend fun getWorkouts(): List<Workout> {
+        TODO("Not yet implemented")
     }
 
     override suspend fun addWorkout(exercises: List<Exercise>, name: String, desc: String) {
@@ -45,6 +59,10 @@ class DatabaseRepositoryImpl @Inject constructor(): DatabaseRepository {
     ) {
         val data = mapOf("exercises" to exercises, "name" to name, "desc" to desc)
         db.collection("workouts").document(workoutId).set(data)
+    }
+
+    override suspend fun getScheduledWorkouts(): List<ScheduledWorkout> {
+        TODO("Not yet implemented")
     }
 
     override suspend fun scheduleWorkout(workoutId: String, date: Date) {
