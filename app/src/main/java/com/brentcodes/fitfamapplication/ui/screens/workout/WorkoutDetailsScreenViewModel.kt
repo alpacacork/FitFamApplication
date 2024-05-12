99427d46c4dfdd2c1089bde3669eb156861674df
Brent <bcen0001@student.monash.edu>
2024-05-12 17:03:25 +1000

    Reposisory adjustments for Firebase and also setting up constructors for Workout Screen related ViewModels

--- app/src/main/java/com/brentcodes/fitfamapplication/ui/screens/workout/WorkoutDetailsScreenViewModel.kt
+++ app/src/main/java/com/brentcodes/fitfamapplication/ui/screens/workout/WorkoutDetailsScreenViewModel.kt
@@ -1,2 +1,15 @@
 package com.brentcodes.fitfamapplication.ui.screens.workout
 
+import androidx.lifecycle.ViewModel
+import com.brentcodes.fitfamapplication.repo.AuthRepository
+import com.brentcodes.fitfamapplication.repo.DatabaseRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class WorkoutDetailsScreenViewModel @Inject constructor(
+    val authRepository: AuthRepository,
+    val dbRepository: DatabaseRepository
+): ViewModel() {
+
+}
