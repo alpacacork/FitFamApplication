d242282daf2a3471f126cf944921ac705f48cc9c
Brent <bcen0001@student.monash.edu>
2024-05-14 23:31:43 +1000

    Put timer in dialog that opens on text selection

--- app/src/main/java/com/brentcodes/fitfamapplication/ui/screens/plan/PlanSchedulePopup.kt
+++ app/src/main/java/com/brentcodes/fitfamapplication/ui/screens/plan/PlanSchedulePopup.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TimeInput
 import androidx.compose.material3.TimePicker
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.material3.rememberTimePickerState
@@ -25,6 +26,10 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import java.time.Instant
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -37,23 +42,30 @@ fun PlanSchedulePopup(modifier: Modifier = Modifier) {
         val timeState = rememberTimePickerState(is24Hour = false)
         val openTimePickerDialog = remember { mutableStateOf(false) }
 
+        //Instant.ofEpochMilli(monthJoinedTimestamp?: 1).atZone(ZoneId.systemDefault())
+        val dateSelected = if (dateState.selectedDateMillis == null) "Unselected" else Instant.ofEpochMilli(dateState.selectedDateMillis!!).atZone(
+            ZoneId.systemDefault()).format(DateTimeFormatter.ISO_LOCAL_DATE)
+        val timeMinutes = timeState.minute
+        val timeHours = timeState.hour
+
         Text(text = "Schedule Workout", fontSize = 32.sp)
         Spacer(modifier = Modifier.height(10.dp))
         Row(verticalAlignment = Alignment.CenterVertically){
             Text("Date:", fontWeight = FontWeight.Bold)
             TextButton(onClick = { openDatePickerDialog.value = true }) {
-                Text(dateState.selectedDateMillis.toString(), color = Color.LightGray, textDecoration = TextDecoration.Underline)
+                Text(dateSelected, color = Color.LightGray, textDecoration = TextDecoration.Underline)
             }
         }
         Row(verticalAlignment = Alignment.CenterVertically){
             Text("Time:", fontWeight = FontWeight.Bold)
             TextButton(onClick = { openTimePickerDialog.value = true }) {
-                Text(timeState.hour.toString(), color = Color.LightGray, textDecoration = TextDecoration.Underline)
+                Text(text = String.format("%02d:%02d", timeHours, timeMinutes), color = Color.LightGray, textDecoration = TextDecoration.Underline)
             }
         }
         
         if (openDatePickerDialog.value) {
             DatePickerDialog(
+                modifier = Modifier.padding(10.dp),
                 onDismissRequest = {
                     openDatePickerDialog.value = false
                 },
@@ -67,7 +79,15 @@ fun PlanSchedulePopup(modifier: Modifier = Modifier) {
             }
         }
         if (openTimePickerDialog.value) {
-            TimePicker(state = timeState)
+            DatePickerDialog(
+                modifier = Modifier.padding(20.dp),
+                onDismissRequest = { openTimePickerDialog.value = false} ,
+                confirmButton = { Button(onClick = { openTimePickerDialog.value = false }) { Text("CONFIRM") } },
+                dismissButton = { Button(onClick = { openTimePickerDialog.value = false }) { Text("CANCEL") } }
+            ) {
+                TimeInput(state = timeState, modifier = Modifier.padding(20.dp))
+            }
+
         }
     }
 }
